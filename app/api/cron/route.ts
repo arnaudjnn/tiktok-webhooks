6fef65fe07e28c1e46bdb567f2e3aaaa7d2f2aa3
Arnaud Jeannin <arnaudjeannin@outlook.com>
2024-12-17 07:24:14 +0000

    fix: prisma connect

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -3,8 +3,6 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const dynamic = "'force-dynamic'";
-
 export async function GET() {
   try {
     const notifications = await prisma.notification.findMany();
